e1b1018d38be056912eda5752a80f1b0c4a2a65d
J0hNnY1ee <leejohnny2024@gmail.com>
2024-07-19 21:09:00 +0800

    initial file

---
+++ src/main/scala/miniRV/Top.scala
@@ -0,0 +1,15 @@
+package miniRV
+
+import chisel3._
+import chisel3.util._
+class TopIO extends Bundle {
+
+}
+class miniRV extends Module {
+  // val io =
+}
+
+
+object cpu extends App {
+    println("fuck")
+}
