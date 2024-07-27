8e04e26ee738621a68ac362a4847451667ce13be
J0hNnY1ee <leejohnny2024@gmail.com>
2024-07-27 21:26:17 +0800

    update

--- src/main/scala/miniRV_pipe_line/Core/pipeline_Regs/IF1_IF2.scala
+++ src/main/scala/miniRV_pipe_line/Core/pipeline_Regs/IF1_IF2.scala
@@ -1,2 +1,47 @@
 package miniRV_pipe_line.Core.pipeline_Regs
 
+import chisel3._
+import chisel3.util._
+import config.Configs._
+import _root_.circt.stage.ChiselStage
+
+class IF1_IF2_IO  extends Bundle {
+    val stall = Input(Bool())
+    val flush = Input(Bool())
+    
+    val commit_if1 = Input(Bool())
+    val pc_if1 = Input(UInt(ADDR_WIDTH.W))
+
+    val commit_if2 = Output(Bool())
+    val pc_if2 = Output(Bool())
+}
+
+class IF1_IF2 extends Module {
+    val io = IO(new IF1_IF2_IO)
+    val pc_if2 = RegInit(START_ADDR.U(ADDR_WIDTH.W))
+    val commit_if2 = RegInit(false.B)
+
+    when(io.flush)
+    {
+        pc_if2 := START_ADDR.U(ADDR_WIDTH.W)
+        commit_if2 := false.B
+    }.elsewhen(!io.stall)
+    {
+        pc_if2 := io.pc_if1
+        commit_if2 := io.commit_if1
+    }
+
+    io.pc_if2 := pc_if2
+    io.commit_if2 := commit_if2
+}
+
+object myIF1_IF2 extends App {
+  println(
+    ChiselStage.emitSystemVerilog(
+      new IF1_IF2 ,
+      firtoolOpts = Array("-disable-all-randomization", "-strip-debug-info")
+    )
+  )
+}
+
+
--- src/main/scala/miniRV_pipe_line/Core/pipeline_Regs/IF2_ID.scala
+++ src/main/scala/miniRV_pipe_line/Core/pipeline_Regs/IF2_ID.scala
@@ -1,2 +1,49 @@
 package miniRV_pipe_line.Core.pipeline_Regs
 
+import chisel3._
+import chisel3.util._
+import config.Configs._
+import _root_.circt.stage.ChiselStage
+
+class IF2_ID_IO extends Bundle {
+  val stall = Input(Bool())
+  val flush = Input(Bool())
+
+  val pc_if2 = Input(UInt(ADDR_WIDTH.W))
+  val inst_if2 = Input(UInt(DATA_WIDTH.W))
+  val commit_if2 = Input(Bool())
+
+  val pc_id = Output(UInt(ADDR_WIDTH.W))
+  val inst_id = Output(UInt(DATA_WIDTH.W))
+  val commit_id = Output(Bool())
+}
+
+class IF2_ID extends Module {
+  val io = IO(new IF2_ID_IO)
+  val pc_id = RegInit(START_ADDR.U(ADDR_WIDTH.W))
+  val inst_id = RegInit(0x13.U(DATA_WIDTH.W))
+  val commit_id = RegInit(false.B)
+
+  when(io.flush) {
+    pc_id := START_ADDR.U
+    inst_id := 0x13.U
+    commit_id := false.B
+  }.elsewhen(!io.stall) {
+    pc_id := io.pc_if2
+    inst_id := io.inst_if2
+    commit_id := io.commit_if2
+  }
+
+  io.pc_id := pc_id
+  io.commit_id := commit_id
+  io.inst_id := inst_id
+}
+
+object myIF2_ID extends App {
+  println(
+    ChiselStage.emitSystemVerilog(
+      new IF2_ID ,
+      firtoolOpts = Array("-disable-all-randomization", "-strip-debug-info")
+    )
+  )
+}
